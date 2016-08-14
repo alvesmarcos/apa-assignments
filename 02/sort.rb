60e03632ecd4a8e20f36b4d76dabf41cb7baaa99
Marcos Alves <alves.henrique.marcos@gmail.com>
2016-08-14 04:31:06 -0300

    Correction in partition method

--- 02/sort.rb
+++ 02/sort.rb
@@ -12,21 +12,21 @@ module Sort
 
 		(head..tail-1).each do |i|
 			if pivot >= list.at(i)
-				list.at(index), list.at(i) = list.at(i), list.at(index)
+				list.get(index).content, list.get(i).content = list.get(i).content, list.get(index).content
 				index+=1
 			end
 		end
-		list.at(tail), list.at(index) = list.at(index), pivot
+		list.get(tail).content, list.get(index).content = list.get(index).content, pivot
 		index
 	end
 
 	# sorting list with quicksort
 	# recursive
-	def quick(list, head, tail)
-		if b < e
+	def quick!(list, head, tail)
+		if head < tail
 			half = partition(list, head, tail)
-			quick(list, head, half-1)
-			quick(list, half+1, tail)
+			quick!(list, head, half-1)
+			quick!(list, half+1, tail)
 		end
 	end
 
