78b27bf3727e03ac02d3a1e40ea1c24698c9128d
Marcos Alves <alves.henrique.marcos@gmail.com>
2016-10-11 22:47:06 -0300

    Refactoring generator

--- dynamic-programming/generator.rb
+++ dynamic-programming/generator.rb
@@ -8,14 +8,14 @@ INPUT = 3
 # define lines input
 LINES = 50
 
-def pascal_triangle()
+def pascal_triangle
 	for i in 1..INPUT
 		# select file name
 		target = open("pascal-triangle/input/00" << i.to_s << ".in", 'w')
 		target.write(LINES.to_s << "\n")
 		# loop
 		for _ in 1..LINES
-			while (n = rand(50)) < (k = rand(50));end
+			while (n = rand(15)) < (k = rand(15));end
 			target.write(n.to_s << " " << k.to_s << "\n")
 		end
 	end
